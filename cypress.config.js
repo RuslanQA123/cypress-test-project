9e97c83bdef6bcf10fefc7df156f98608d53b4b8
Ruslan Korniienko <ruslan.kornienko.2013@gmail.com>
2025-10-05 18:47:44 +0300

    added tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space',
+    retries: {
+      runMode: 1,
+      openMode: 1,
+    }
     },
      viewportWidth: 1280,
     viewportHeight: 720,
     defaultCommandTimeout: 8000,
+    pageLoadTimeout: 30000,
     video: false,
+    screenshotOnRunFailure: true,
     screenshotsFolder: 'cypress/screenshots',
     videosFolder: 'cypress/videos'
   },
-});
+);
